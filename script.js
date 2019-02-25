c181e8da2cb4953cc3d4bd626239132ea083d563
Lorenzo Ercoli <l.ercoli@outlook.it>
2019-02-25 17:33:46 +0100

    a

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+var modal = document.getElementById('modal01');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var captionText = document.getElementById("caption");
+
+
 function onClick(element) {
 	document.getElementById("img01").src = element.src;
 	document.getElementById("modal01").style.display = "block";
+	captionText.innerHTML = element.alt;
 }
+
